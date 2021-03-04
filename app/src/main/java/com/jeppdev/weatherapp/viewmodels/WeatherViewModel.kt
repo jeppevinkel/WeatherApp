2cfd548595001afd31d84201e424a94c8a34cbac
Claus Huynh <clhuy18@student.sdu.dk>
2021-03-04 16:04:19 +0100

    add fuzzy logic

--- app/src/main/java/com/jeppdev/weatherapp/viewmodels/WeatherViewModel.kt
+++ app/src/main/java/com/jeppdev/weatherapp/viewmodels/WeatherViewModel.kt
@@ -2,16 +2,15 @@ package com.jeppdev.weatherapp.viewmodels
 
 import android.app.Application
 import android.content.Context
+import android.content.res.Resources
 import android.util.Log
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.jeppdev.weatherapp.database.AppDatabase
 import com.jeppdev.weatherapp.database.WeatherData
+import com.jeppdev.weatherapp.R
+import com.jeppdev.weatherapp.database.AppDatabase
 import com.jeppdev.weatherapp.models.WeatherModel
 
 class WeatherViewModel(application: Application) : AndroidViewModel(application) {
@@ -21,6 +20,71 @@ class WeatherViewModel(application: Application) : AndroidViewModel(application)
     private lateinit var db: AppDatabase
 
     private val weather = MutableLiveData<WeatherData>()
+    private val fuzzyWeatherValue = arrayOf(
+        application.getString(R.string.frigid),
+        application.getString(R.string.freezing),
+        application.getString(R.string.cold),
+        application.getString(R.string.cool),
+        application.getString(R.string.mild),
+        application.getString(R.string.warm),
+        application.getString(R.string.hot),
+        application.getString(R.string.sweltering)
+    )
+    private val clothing = arrayOf(
+        //clothing when frigid
+        arrayOf(
+            application.getString(R.string.jacket),
+            application.getString(R.string.mittens),
+            application.getString(R.string.scarf),
+            application.getString(R.string.socks),
+            application.getString(R.string.warm_hat),
+            application.getString(R.string.thick_pants)
+        ),
+        //clothing when freezing
+        arrayOf(
+            application.getString(R.string.jacket),
+            application.getString(R.string.mittens),
+            application.getString(R.string.socks),
+            application.getString(R.string.warm_hat),
+            application.getString(R.string.thick_pants)
+        ),
+        //clothing when cold
+        arrayOf(
+            application.getString(R.string.jacket),
+            application.getString(R.string.socks),
+            application.getString(R.string.warm_hat),
+            application.getString(R.string.jeans)
+        ),
+        //clothing when cool
+        arrayOf(
+            application.getString(R.string.jacket),
+            application.getString(R.string.socks),
+            application.getString(R.string.jeans)
+        ),
+        //clothing when mild
+        arrayOf(
+            application.getString(R.string.jacket),
+            application.getString(R.string.socks),
+            application.getString(R.string.jeans)
+        ),
+        //clothing when warm
+        arrayOf(
+            application.getString(R.string.t_shirt),
+            application.getString(R.string.jeans)
+        ),
+        //clothing when hot
+        arrayOf(
+            application.getString(R.string.t_shirt),
+            application.getString(R.string.shorts)
+        ),
+        //clothing when sweltering
+        arrayOf(
+            application.getString(R.string.t_shirt),
+            application.getString(R.string.jeans)
+        )
+    )
+
+
     val queue = Volley.newRequestQueue(application.applicationContext)
 
     init {
@@ -96,5 +160,44 @@ class WeatherViewModel(application: Application) : AndroidViewModel(application)
 //    }
     fun getWeather() : LiveData<WeatherData> {
         return weather
+    }
+
+    /* here begins fuzzy stuff */
+
+    fun getClothing(temperature: Double) : String {
+        var returnString = ""
+
+        val arrayOfClothing = clothing[getFuzzyIndex(temperature)]
+        val listSize = arrayOfClothing.size
+        for (index in 0 .. listSize-3){
+            returnString = returnString + arrayOfClothing[index] + ", "
+        }
+        returnString += arrayOfClothing[listSize-2] + " " + "and"  + " " + arrayOfClothing[listSize-1] //skriver and indtil getString virker
+        return returnString
+
+    }
+
+    fun getFuzzyTemperature(temperature: Double) : String{
+        return fuzzyWeatherValue[getFuzzyIndex(temperature)]
+    }
+
+    private fun getFuzzyIndex(temperature: Double) : Int {
+        if (temperature <= -3.25)
+            return 0
+        else if (-3.25 < temperature && temperature <= 3.13)
+            return 1
+        else if (3.13 < temperature && temperature <= 7.25)
+            return 2
+        else if (7.25 < temperature && temperature <= 14.13)
+            return 3
+        else if (14.13 < temperature && temperature <= 20.75)
+            return 4
+        else if (20.75 < temperature && temperature <= 26.5)
+            return 5
+        else if (26.5 < temperature && temperature <= 32)
+            return 6
+        else if (32 < temperature)
+            return 7
+        else return -1
     }
 }
--- app/src/main/java/com/jeppdev/weatherapp/views/MainActivity.kt
+++ app/src/main/java/com/jeppdev/weatherapp/views/MainActivity.kt
@@ -27,6 +27,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var feelsLikeTextView: TextView
     private lateinit var locationTextView: TextView
 //    private lateinit var locationManager: LocationManager
+    private lateinit var fuzzyTemperatureTextView: TextView
+    private lateinit var recommendedClothTextView: TextView
+
     private val weatherViewModel: WeatherViewModel by viewModels()
     private val gpsViewModel: GpsViewModel by viewModels()
 
@@ -37,6 +40,8 @@ class MainActivity : AppCompatActivity() {
         temperatureTextView = findViewById(R.id.temperature_text)
         feelsLikeTextView = findViewById(R.id.feels_like_text)
         locationTextView = findViewById(R.id.location_text)
+        fuzzyTemperatureTextView = findViewById(R.id.fuzzy_temperature_text)
+        recommendedClothTextView = findViewById(R.id.recommended_clothing_text)
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), gpsViewModel.PERMISSION_ID)
@@ -54,6 +59,13 @@ class MainActivity : AppCompatActivity() {
             locationTextView.text = ("Latitude: %s\nLongitude: %s".format(location.latitude, location.longitude))
             if (location.latitude != null && location.longitude != null) weatherViewModel.updateWeather(location.latitude, location.longitude)
             Log.d("WEATHER_LOG", "Location changed!")
+        })
+
+
+        //fuzzy test
+        weatherViewModel.getWeather().observe(this, { weather ->
+            fuzzyTemperatureTextView.text = weatherViewModel.getFuzzyTemperature(weather.feelsLike - 273.15)
+            recommendedClothTextView.text = weatherViewModel.getClothing(weather.feelsLike - 273.15)
         })
 
     }
