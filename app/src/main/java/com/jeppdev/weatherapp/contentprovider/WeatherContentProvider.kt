b385b002aaa1fbad7e55ff286335cda9fe922a70
Jeppevinkel <jeppevinkel@gmail.com>
2021-03-18 14:21:30 +0100

    Update content provider parameter names

--- app/src/main/java/com/jeppdev/weatherapp/contentprovider/WeatherContentProvider.kt
+++ app/src/main/java/com/jeppdev/weatherapp/contentprovider/WeatherContentProvider.kt
@@ -7,26 +7,26 @@ import android.net.Uri
 
 class WeatherContentProvider : ContentProvider() {
     override fun onCreate(): Boolean {
-        TODO("Not yet implemented")
+        TODO("Implement onCreate")
     }
 
-    override fun query(p0: Uri, p1: Array<out String>?, p2: String?, p3: Array<out String>?, p4: String?): Cursor? {
-        TODO("Not yet implemented")
+    override fun query(uri: Uri, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor? {
+        TODO("Implement query")
     }
 
-    override fun getType(p0: Uri): String? {
-        TODO("Not yet implemented")
+    override fun getType(uri: Uri): String? {
+        TODO("Implement getType")
     }
 
-    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
-        TODO("Not yet implemented")
+    override fun insert(uri: Uri, contentValues: ContentValues?): Uri? {
+        TODO("Implement insert")
     }
 
-    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
-        TODO("Not yet implemented")
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        TODO("Implement delete")
     }
 
-    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
-        TODO("Not yet implemented")
+    override fun update(uri: Uri, contentValues: ContentValues?, selection: String?, selectionArgs: Array<out String>?): Int {
+        TODO("Implement update")
     }
 }
